6d96da5e839c3243bab9f8a5299d3a836eac1b3d
Lee Jeong Seo <wjdtj35@gmail.com>
2024-02-01 23:30:25 +0900

    first backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Box, TextField, Button, List, ListItem, ListItemText, Stack } from '@mui/material';
 
-function App() {
+function TodoList() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box className="headContainer">
+        <Box>
+          <h1>Todo List</h1>
+          <Stack spacing={2} direction="row">
+            <TextField 
+              id="standard-basic" 
+              label="New Todo" 
+              variant="standard" 
+              sx={{ width: '80%' }}
+            />
+            <Button className="btns" variant="outlined"
+            sx={{
+              color: '#FFF',
+              borderColor: '#FFF',
+              '&:hover': {
+                backgroundColor: '#7DBFD8', // hover 시 배경색 변경
+                color: '#FFF', // hover 시 텍스트 색상 변경
+              }
+            }}
+            >Todo</Button>
+          </Stack>
+        </Box>
+      </Box>
+      <Box className="boxContainer">
+          여기에다 글적어
+        </Box>
+    </Box>
   );
 }
 
-export default App;
+export default TodoList;
